84a27880f09e05243d779e28e029e55d840461e4
LuizaGA <89354119+LuizaGA@users.noreply.github.com>
2021-11-22 16:41:51 -0300

    layout melhorado

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -4,24 +4,26 @@ import "./styles.css";
 export default function Adicionar() {
   return (
     <>
-      <form>
-        <h1>Adicionar Compras</h1>
-        <div>
-          <h2> Marca da Ração:</h2>
-          <input placeholder="marca da ração"></input>
-        </div>
-        <div>
-          <h2>Quantidade (kg): </h2>
-          <input type="number" placeholder="quantidade da ração"></input>
-        </div>
-        <div>
-          <h2>Preço (R$): </h2>
-          <input type="number" placeholder="preço da ração"></input>
-        </div>
-        <button type="submit">
-          <h3>Adiconar Compra</h3>
-        </button>
-      </form>
+      <div className="container">
+        <form>
+          <h1>Adicionar Compras</h1>
+          <div>
+            <label> Marca da Ração:</label>
+            <input placeholder="Marca X"></input>
+          </div>
+          <div>
+            <label>Quantidade (kg): </label>
+            <input type="number" placeholder="20"></input>
+          </div>
+          <div>
+            <label>Preço (R$): </label>
+            <input type="number" placeholder="100"></input>
+          </div>
+          <button type="submit">
+            <h3>Adiconar Compra</h3>
+          </button>
+        </form>
+      </div>
     </>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React; from 'react'
+import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Adicionar from './Pages/Adicionar';
